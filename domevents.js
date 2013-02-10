2c5fd578387dff015c7c8c4c59d62142571c6aa7
srikanth <srikanth@srikanth-Lenovo-Z580>
2013-02-10 00:27:26 -0600

    Add Task Functionality completed

--- domevents.js
+++ domevents.js
@@ -1,19 +1,33 @@
-taskList = {Task3 : 5, Task5: 4, Task4 : 3};
+allTasks = {"list1":{Task3 : 5, Task5: 4, Task4 : 3}};
+listIdentifier="list1";
 function getTasksHTMLRepresentation() {
 	var result= '<table id="task-list"/>';
+	var taskList = allTasks[listIdentifier];
 	// comparator for sorting based on dates
 	var tasks = Object.keys(taskList).sort(function(a, b) {return taskList[a] - taskList[b];});
 	for(var i = 0; i < tasks.length; i++)
-	    result = result + '<tr><td><input type="checkbox" value="'+tasks[i]+'"/>'+ tasks[i] +'</td></tr>';
+	    result = result + '<tr><td><input type="checkbox" id="'+tasks[i]+'"/>'+ tasks[i] +'</td></tr>';
     result = result + '</table>';
     return result;
 }
+
+function addTask(taskDescription) {
+    (allTasks[listIdentifier])[taskDescription] = 6;
+}
+
+function deleteTask(taskDescription) {
+	delete (allTasks[listIdentifier])[key];
+}
+
 $(document).ready(function() { 
     var content = getTasksHTMLRepresentation();
     var e = $('#task-list').html(content);
+    
     $("#add-task").on("click", function() {
-            var value = $("#user-input").val(); 
-            var e = $('#task-list').html("");
+            var taskDescription = $("#user-input").val();
+            addTask(taskDescription);
+            var modifiedHTML = getTasksHTMLRepresentation();
+            var e = $('#task-list').html(modifiedHTML);
     });
     
     $(".rhs-bottom-box").on("click", function() {
